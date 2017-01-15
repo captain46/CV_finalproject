40c8b9e26f9793fa1686b4460a8a66867c925f8a
Simone <simone.hierhold@edu.fh-joanneum.at>
2017-01-15 15:34:18 +0100

    Initial commit

---
+++ src/main/java/es/uah/ComputervisionApplication.java
@@ -0,0 +1,12 @@
+package es.uah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputervisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputervisionApplication.class, args);
+	}
+}
